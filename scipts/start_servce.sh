6a7cb929665aa5a7bfe63ea4f7c8e682bbafd7fa
suriya1701 <141544638+suriya1701@users.noreply.github.com>
2024-09-09 15:09:19 +0530

    Update start_servce.sh

--- scipts/start_servce.sh
+++ scipts/start_servce.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-cd /home/ec2-user/node-app
+
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ec2-user/techflare-backend
+
+#navigate into our working directory where we have all our github files
+cd /home/ec2-user/techflare-backend
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm" 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+#install node modules
 npm install
-pm2 start app.js
+
+#start our node app in the background
+npm start > app.out.log 2> app.err.log < /dev/null & 
